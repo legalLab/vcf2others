83053084d5062b76697f6537cb68659fd695012b
killidude <hrbek@evoamazon.net>
2024-11-02 23:49:25 -0500

    cosmetic change to output file name

--- R/vcf_stats.R
+++ R/vcf_stats.R
@@ -3,7 +3,7 @@
 #' @description high heterozygosity is indicative of potential contamination
 #' @description average read depth per individual
 #' @description missing data per individual
-#' 
+#'
 #' @author Tomas Hrbek August 2024
 #'
 #' @param vcf -> vcfR object
@@ -12,7 +12,7 @@
 #'
 #' @details
 #' This function calculates average read depth, heterozygosity
-#' number of heterozygotes, number of reference and alternative homozygotes, 
+#' number of heterozygotes, number of reference and alternative homozygotes,
 #' missing data and total number SNPs of each sample in an vcfR object
 #'
 #' @examples
@@ -38,17 +38,17 @@ vcf_stats <- function(vcf, res_path, project) {
   total <- homo_ref+homo_alt+hetero
   loc_n <- apply(gt, MARGIN = 2, function(x){length(x)})
   depth <- apply(dp, MARGIN = 2, function(x){mean(x)})
-  
-  stats <- rbind(samples, depth, hetero/total, hetero, (homo_ref+homo_alt), 
+
+  stats <- rbind(samples, depth, hetero/total, hetero, (homo_ref+homo_alt),
                  homo_ref, homo_alt, miss/loc_n, miss, total, loc_n) %>%
     t() %>%
     as.data.frame()
   colnames(stats) <- c('id', 'read_depth', 'heterozygosity', 'heterozygotes', 'homozygotes',
-                       'homozygotes_ref', 'homozygotes_alt', 'missing_p', 'missing', 
+                       'homozygotes_ref', 'homozygotes_alt', 'missing_p', 'missing',
                       'non_missing', 'total')
-  
-  write.table(stats, file = paste0(res_path, project, "stats.csv"), row.names = FALSE, quote = FALSE, sep = ",")
-  
+
+  write.table(stats, file = paste0(res_path, project, "_stats.csv"), row.names = FALSE, quote = FALSE, sep = ",")
+
   # invisible return of the first argument so function can be used in a pipe
   invisible(vcf)
 }
