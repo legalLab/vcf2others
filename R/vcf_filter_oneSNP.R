65e108fd0527daa5f076dcf57c26fdb153dd4dd9
killidude <hrbek@evoamazon.net>
2025-03-23 23:19:28 -0400

    adding functionality to filter one SNP per chromosome per block

--- R/vcf_filter_oneSNP.R
+++ R/vcf_filter_oneSNP.R
@@ -3,24 +3,43 @@
 #' @author Tomas Hrbek February 2022
 #'
 #' @param vcf -> vcfR object
+#' @param block_size -> size of linked SNP blocks (integer)
 #' @export
 #' @return subsetted vcfR object
 #'
 #' @details
 #' This function subsets the vcfR object keeping only 1 SNP per locus, returning new vcfR object
-#' The first SNP independent of quality is taken (may mofify this in future)
+#' Locus is defined as a different chromosome or a block within a chromosome
+#' The first SNP independent of quality is taken (may mofify this in the future)
 #'
 #' @examples
 #' vcf_filter_oneSNP(vcf = my_vcf)
+#' vcf_filter_oneSNP(my_vcf, block_size = 1000)
 #' vcf_filter_oneSNP(my_vcf)
 #'
 
-vcf_filter_oneSNP <- function(vcf) {
-  # read all loci names in vcf
-  chrom <- vcfR::getCHROM(vcf)
+vcf_filter_oneSNP <- function(vcf, block_size = 10000) {
+  # read all loci names and positions of their SNPs in VCF
+  chrom_pos_table <- arrow::Table$create(
+    chrom = vcfR::getCHROM(vcf),
+    pos = vcfR::getPOS(vcf),
+    index = 1:length(vcfR::getCHROM(vcf))
+  )
 
-  # keep only those loci with 1 SNP
-  vcf <- vcf[!duplicated(chrom), ]
+  # compute blocks within each chromosome
+  selected_indices <- chrom_pos_table %>%
+    dplyr::group_by(chrom) %>%
+    dplyr::mutate(first_pos = min(pos)) %>%
+    dplyr::mutate(block = ((pos - first_pos) %/% block_size) + 1) %>%
+    dplyr::ungroup() %>%
+    dplyr::arrange(chrom, block, pos) %>%
+    # select the first SNP from each chromosome/block combination
+    dplyr::distinct(chrom, block, .keep_all = TRUE) %>%
+    dplyr::collect() %>%
+    dplyr::pull(index)
+
+  # filter VCF to keep only selected SNPs
+  vcf <- vcf[selected_indices, ]
 
   return(vcf)
 }
